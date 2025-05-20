609452bc56ed96159d5214c2c720822326618879
cocomongg <pjm4142@naver.com>
2025-05-20 23:51:17 +0900

    feat: health-check, welcome api 추가

---
+++ user-service/src/main/java/com/example/userservice/vo/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.userservice.vo;
+
+import lombok.Getter;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class Greeting {
+
+    @Value("${service.greeting.message}")
+    private String message;
+}
